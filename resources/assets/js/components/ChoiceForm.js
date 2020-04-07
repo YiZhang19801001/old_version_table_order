ab066b041f97fc03a67fd5c2db66fb00b43e7829
roben <yizhang19801001@gmail.com>
2020-04-08 08:15:56 +1000

    remove unused console.log

--- resources/assets/js/components/ChoiceForm.js
+++ resources/assets/js/components/ChoiceForm.js
@@ -74,8 +74,6 @@ export default class ChoiceForm extends Component {
   updateOrderItemChoice(pickedChoice, action) {
     console.log({ pickedChoice, action });
     if (action) {
-      console.log(pickedChoice.type != 8889);
-
       this.setState({
         pickedChoice: pickedChoice.type != 8889
           ? [...this.state.pickedChoice.filter(pc => pc.type != pickedChoice.type), pickedChoice]
